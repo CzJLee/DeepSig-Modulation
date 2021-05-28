8a5164352655cc122cf16ac5ffb425d427e659f3
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2021-05-28 11:57:31 -0700

    Add new models to test

--- train_model_iq.py
+++ train_model_iq.py
@@ -37,8 +37,9 @@ labels_test = np.load(os.path.join(dataset_directory, "iq_labels_test.npy"))
 # labels_test = np.load(os.path.join(dataset_directory, "labels_test.npy"), mmap_mode="r+")
 
 
-# # %% This model scored 89% accuracy
+# %%
 # # IQ Smaller CNN Model
+# # This model scored 89% accuracy
 # inputs = keras.Input(shape=(1024, 2))
 # x = layers.Conv1D(filters=64, kernel_size=3, activation='relu')(inputs)
 # x = layers.BatchNormalization()(x)
@@ -71,8 +72,13 @@ labels_test = np.load(os.path.join(dataset_directory, "iq_labels_test.npy"))
 
 # model.summary()
 
+
 # %%
-# IQ Smaller CNN Model
+models = []
+
+
+# %%
+# IQ Larger Kernel
 inputs = keras.Input(shape=(1024, 2))
 x = layers.Conv1D(filters=64, kernel_size=7, activation='relu')(inputs)
 x = layers.BatchNormalization()(x)
@@ -89,7 +95,7 @@ x = layers.MaxPooling1D(pool_size=2)(x)
 x = layers.Conv1D(filters=64, kernel_size=5, activation='relu')(x)
 x = layers.BatchNormalization()(x)
 x = layers.MaxPooling1D(pool_size=2)(x)
-x = layers.Conv1D(filters=64, kernel_size=5, activation='relu')(x)
+x = layers.Conv1D(filters=64, kernel_size=3, activation='relu')(x)
 x = layers.BatchNormalization()(x)
 x = layers.MaxPooling1D(pool_size=2)(x)
 x = layers.Conv1D(filters=64, kernel_size=3, activation='relu')(x)
@@ -98,9 +104,32 @@ x = layers.Flatten()(x)
 x = layers.Dense(128)(x)
 outputs = layers.Dense(24, activation='softmax')(x)
 
-model = keras.Model(inputs=inputs, outputs=outputs, name = "iq_even_smaller_model")
+models.append(keras.Model(inputs=inputs, outputs=outputs, name = "iq_even_smaller_model_larger_kernel"))
 
-model.summary()
+models[-1].summary()
+
+
+# %%
+# IQ Larger Kernel
+inputs = keras.Input(shape=(1024, 2))
+x = layers.Conv1D(filters=64, kernel_size=7, activation='relu')(inputs)
+x = layers.MaxPooling1D(pool_size=2)(x)
+x = layers.Conv1D(filters=64, kernel_size=7, activation='relu')(x)
+x = layers.MaxPooling1D(pool_size=2)(x)
+x = layers.Conv1D(filters=64, kernel_size=5, activation='relu')(x)
+x = layers.MaxPooling1D(pool_size=2)(x)
+x = layers.Conv1D(filters=64, kernel_size=5, activation='relu')(x)
+x = layers.MaxPooling1D(pool_size=2)(x)
+x = layers.Conv1D(filters=64, kernel_size=5, activation='relu')(x)
+x = layers.MaxPooling1D(pool_size=2)(x)
+x = layers.Conv1D(filters=64, kernel_size=3, activation='relu')(x)
+x = layers.Flatten()(x)
+x = layers.Dense(128)(x)
+outputs = layers.Dense(24, activation='softmax')(x)
+
+models.append(keras.Model(inputs=inputs, outputs=outputs, name = "iq_even_smaller_model_larger_kernel_no_normalization"))
+
+models[-1].summary()
 
 
 # %%
@@ -115,31 +144,32 @@ model.summary()
 
 
 # %%
-# Time the time it takes to train the model
-start = time.time()
+for model in models:
+    # Time the time it takes to train the model
+    start = time.time()
 
-# Compile model
-model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
 
-# Define the callbacks and save the best model to a new file
-callbacks = [keras.callbacks.ModelCheckpoint(filepath=f'models/{model.name}.keras', save_best_only=True, monitor='val_loss'), 
-    keras.callbacks.EarlyStopping(monitor="val_accuracy", min_delta = 0.0001, patience = 10, verbose = 1, restore_best_weights = True)]
-# Train model
-history = model.fit(dataset_train, labels_train, epochs=200, batch_size = 256, validation_data = (dataset_val, labels_val), callbacks=callbacks)
+    # Define the callbacks and save the best model to a new file
+    callbacks = [keras.callbacks.ModelCheckpoint(filepath=f'models/{model.name}.keras', save_best_only=True, monitor='val_loss'), 
+        keras.callbacks.EarlyStopping(monitor="val_accuracy", min_delta = 0.0001, patience = 10, verbose = 1, restore_best_weights = True)]
+    # Train model
+    history = model.fit(dataset_train, labels_train, epochs=200, batch_size = 256, validation_data = (dataset_val, labels_val), callbacks=callbacks)
 
-# Save pickle of history
-with open(f"models/{model.name}.pickle", mode='wb') as f:
-    pickle.dump(history.history, f)
+    # Save pickle of history
+    with open(f"models/{model.name}.pickle", mode='wb') as f:
+        pickle.dump(history.history, f)
 
-# Evaluate model
-_, accuracy = model.evaluate(dataset_test, labels_test)
+    # Evaluate model
+    _, accuracy = model.evaluate(dataset_test, labels_test)
 
-print("--- Model trained in %s seconds ---" % (time.time() - start))
+    print("--- Model trained in %s seconds ---" % (time.time() - start))
 
-# Write accuracy to file
-f = open("models/accuracy_log.txt", "a")
-f.write(f"Model {model.name} evaluated an accuracy of {accuracy}.\n")
-f.write(f"Model {model.name} trained in {int(time.time() - start)} seconds.\n\n")
-f.close()
+    # Write accuracy to file
+    f = open("models/accuracy_log.txt", "a")
+    f.write(f"Model {model.name} evaluated an accuracy of {accuracy}.\n")
+    f.write(f"Model {model.name} trained in {int(time.time() - start)} seconds.\n\n")
+    f.close()
 
 
